4f4e7173901625bee2220d8fa7e8c207c8a5df5e
no.6 <yhs1395045@gmail.com>
2018-11-04 15:42:15 +0800

    init Network

---
+++ Network/Router.cpp
@@ -0,0 +1,573 @@
+#include "stdafx.h"
+#include <thread>
+#include <memory>
+#include <list>
+#include <algorithm>
+#include <condition_variable>
+#include "Router.h"
+
+namespace Http
+{
+
+	Memory::Memory()
+	{
+		Size(0);    /* no data at this point */
+		MemoryAddr((char *)malloc(1));  /* will be grown as needed by the realloc above */
+	}
+
+	Memory::Memory(const Memory &memory)
+		:size(memory.size), memoryAddr(nullptr)
+	{
+		Size(memory.Size());
+		memoryAddr = (char *)malloc(memory.Size());
+		memcpy(memoryAddr, memory.MemoryAddr(), memory.Size());
+	}
+
+	Memory::Memory(Memory &&memory)
+		:memoryAddr(memory.memoryAddr), size(memory.size)
+	{
+		memory.MemoryAddr(nullptr);
+	}
+
+	bool Memory::operator==(const Memory&memory)const
+	{
+		return memoryAddr == memory.memoryAddr && size == memory.size;
+	}
+
+
+	char * Memory::MemoryAddr() const
+	{
+		return memoryAddr;
+	}
+
+
+	void Memory::MemoryAddr(char * val)
+	{
+		memoryAddr = val;
+	}
+
+
+	size_t Memory::Size() const
+	{
+		return size;
+	}
+
+
+	void Memory::Size(size_t val)
+	{
+		size = val;
+	}
+
+	Memory::~Memory()
+	{
+		free(MemoryAddr());
+		MemoryAddr(0);
+	}
+
+	Request::Request(const std::string &url, void * userData) :url(url), userData(userData), unhandled(true), uploadeddatas(), type(TYPE::GET)
+	{}
+
+	Request::Request(const std::string &url, const std::vector<UploadedData> &uploadeddatas, void *userData /*= nullptr*/)
+		: url(url), uploadeddatas(uploadeddatas), userData(userData), type(TYPE::POST)
+	{
+
+	}
+
+	Request::Request(const Request &request) 
+		: url(request.Url()), userData(request.UserData()),
+		unhandled(request.Unhandled()), uploadeddatas(request.uploadeddatas), type(request.type)
+	{}
+
+
+	Request::Request(Request &&request)
+		: url(std::move(request.url)), userData(request.userData), 
+		unhandled(request.unhandled), uploadeddatas(std::move(request.uploadeddatas)), type(request.type)
+	{
+		request.UserData(nullptr);
+	}
+
+
+
+	bool Request::operator==(const Request&request) const
+	{
+		return url == request.url&&unhandled == request.unhandled&&userData == request.userData && uploadeddatas == request.uploadeddatas&&type==request.Type();
+	}
+
+	Request::~Request()
+	{
+		delete userData;
+		UserData(0);
+	}
+
+
+	NETWORK_API const std::string & Request::Url() const
+	{
+		return url;
+	}
+
+
+	NETWORK_API void Request::Url(const std::string &val)
+	{
+		url = val;
+	}
+
+
+	NETWORK_API void Request::Unhandled(bool)
+	{
+		unhandled = false;
+	}
+
+
+	NETWORK_API bool Request::Unhandled() const
+	{
+		return unhandled;
+	}
+
+
+	NETWORK_API void * Request::UserData() const
+	{
+		return userData;
+	}
+
+
+	NETWORK_API void Request::UserData(void * val)
+	{
+		userData = val;
+	}
+
+
+	NETWORK_API std::vector<Http::UploadedData> & Request::Uploadeddatas()
+	{
+		return uploadeddatas;
+	}
+
+
+	NETWORK_API Http::Request::TYPE Request::Type() const
+	{
+#ifdef _DEBUG
+		assert(type == GET || type == POST);
+#endif
+		return type;
+	}
+
+
+	NETWORK_API void Request::Type(TYPE val)
+	{
+		type = val; 
+#ifdef _DEBUG
+		assert(type == GET || type == POST);
+#endif
+	}
+
+	Response::Response() :Memory(), curlCode(CURLE_OK), curl(nullptr) {}
+
+	Response::Response(const Response &response) : Memory(response), curlCode(response.CurlCode()), curl(response.Curl()) {}
+
+
+	Response::Response(Response &&response)
+		: curlCode(response.curlCode), curl(response.curl), Memory(std::move(response))
+	{
+
+	}
+
+
+	CURLcode Response::CurlCode() const
+	{
+		return curlCode;
+	}
+
+
+	void Response::CurlCode(CURLcode val)
+	{
+		curlCode = val;
+	}
+
+
+	CURL * Response::Curl() const
+	{
+		return curl;
+	}
+
+
+	void Response::Curl(CURL * val)
+	{
+		curl = val;
+	}
+
+	bool Response::operator==(const Response &&response)const
+	{
+		if (Memory::operator==(static_cast<const Memory &&>(response)))
+		{
+			return curlCode == response.curlCode&&curl == response.curl;
+		}
+		return false;
+	}
+
+	Http::Router & Router::GetInstance()
+	{
+		static Router instance;
+		return instance;
+	}
+
+	Task::Task(Task &&task) :
+		Request(std::move(static_cast<Request&>(task))), Response(std::move(static_cast<Response&>(task))),
+		action(task.Action()), mark(task.Mark())
+	{
+		task.Action(nullptr);
+	}
+
+
+	Task::Task(const Task &task) : Request(task), Response(task),
+		action(task.action), mark(task.mark)
+	{
+
+	}
+
+
+	Task::Task(const std::string &url, Http::Action *action, void * userdata /*= nullptr*/) :Request(url, userdata), Response(), action(action), mark(Task::markCouter++)
+	{
+
+	}
+
+	Task::Task(const std::string &url, const std::vector<UploadedData> &uploadData, Http::Action *action, void *userData /*= nullptr*/) : Request(url, uploadData, userData), action(action), mark(Task::markCouter++)
+	{
+
+	}
+
+	bool Task::operator==(const Task &task) const
+	{
+		if (Request::operator==(static_cast<const Task&&>(task)) &&
+			Response::operator==(static_cast<const Task&&>(task)))
+		{
+			return mark == task.mark && action == task.action;
+		}
+		return false;
+	}
+
+	Http::Action * Task::Action() const
+	{
+		return action;
+	}
+
+	void Task::Action(Http::Action * val)
+	{
+		action = val;
+	}
+
+	long long Task::Mark() const
+	{
+		return mark;
+	}
+
+	void Task::Mark(long long val)
+	{
+		mark = val;
+	}
+
+	long long Task::markCouter = 0;
+	volatile  bool g_createdExcutor = true;
+
+	/*taskQueue maintains a task queue to perform task orderly*/
+	class TaskQueue {
+	public:
+		std::list<Task>::iterator FrontUnhandledTask() {
+			for (auto beg = std::begin(taskQueue); beg != std::end(taskQueue); ++beg)
+			{
+				if (beg->Unhandled())
+				{
+					return beg;
+				}
+			}
+			return taskQueue.end();
+		}
+		bool HasUnhandledTask() {
+			return !(FrontUnhandledTask() == taskQueue.end());
+		}
+
+		void Push(Task &&task) {
+			taskQueue.push_back(std::move(task));
+		}
+		void Pop(long long mark) {
+			taskQueue.remove_if([mark](const Task &task) {
+				return mark == task.Mark();
+			});
+		}
+	private:
+		std::list<Task> taskQueue;
+	};
+	TaskQueue g_taskQueue;
+	/*atomic variable determines TaskExcuter Thread Living Status*/
+
+	static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+	{
+		size_t realsize = size * nmemb;
+		Task *l_memory = (Task *)userp;
+
+		l_memory->MemoryAddr((char *)realloc(l_memory->MemoryAddr(), l_memory->Size() + realsize + 1));
+		if (l_memory->MemoryAddr() == NULL)
+			return 0;
+
+		memcpy(&(l_memory->MemoryAddr()[l_memory->Size()]), contents, realsize);
+		l_memory->Size(l_memory->Size() + realsize);
+		l_memory->MemoryAddr()[l_memory->Size()] = 0;
+
+		return realsize;
+	}
+
+	static int xferinfo(void *p, curl_off_t dltotal, curl_off_t dlnow, curl_off_t ultotal, curl_off_t ulnow)
+	{
+		double curtime = 0;
+		Task *task = (Task *)p;
+		curl_easy_getinfo(task->Curl(), CURLINFO_TOTAL_TIME, &curtime);
+		return task->Action()->Progress(curtime, (double)dltotal, (double)dlnow, 0, 0, *task);
+	}
+
+	static int older_progress(void *p, double dltotal, double dlnow, double ultotal, double ulnow)
+	{
+		return xferinfo(p, (curl_off_t)dltotal, (curl_off_t)dlnow, (curl_off_t)ultotal, (curl_off_t)ulnow);
+	}
+
+
+	static void init(CURLM *cm)
+	{
+		CURL *eh = curl_easy_init();
+		Task& unhandledTask = *g_taskQueue.FrontUnhandledTask();
+		unhandledTask.Curl(eh);
+		//check request type
+		Request::TYPE type = unhandledTask.Type();
+		if (unhandledTask.Type() == Request::TYPE::POST)
+		{
+			struct curl_httppost *formpost = NULL;
+			struct curl_httppost *lastptr = NULL;
+
+			struct curl_slist *headerlist = NULL;
+			static const char buf[] = "Expect:";
+
+			const std::vector<UploadedData> &uploadedDatas = unhandledTask.Uploadeddatas();
+			for (auto data : uploadedDatas)
+			{
+				CURLformoption opt;
+				if (data.Field() == UploadedData::FIELD::FILE)
+				{
+					opt = CURLformoption::CURLFORM_FILE;
+				}
+				else
+				{
+					opt = CURLformoption::CURLFORM_COPYCONTENTS;
+				}
+				curl_formadd(&formpost, &lastptr,
+					CURLFORM_COPYNAME, data.Key().c_str(),
+					opt, data.Value().c_str(),
+					CURLFORM_END);
+			}
+			headerlist = curl_slist_append(headerlist, buf);
+			curl_easy_setopt(eh, CURLOPT_HTTPHEADER, headerlist);
+			curl_easy_setopt(eh, CURLOPT_HTTPPOST, formpost);
+		}
+		//set easy handle option
+		curl_easy_setopt(eh, CURLOPT_PRIVATE, (void *)&unhandledTask);
+		curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_setopt(eh, CURLOPT_WRITEDATA, (void *)&unhandledTask);
+		curl_easy_setopt(eh, CURLOPT_VERBOSE, 0L);
+		curl_easy_setopt(eh, CURLOPT_NOPROGRESS, 0);
+#if LIBCURL_VERSION_NUM >= 0x072000
+		curl_easy_setopt(eh, CURLOPT_XFERINFOFUNCTION, xferinfo);
+		curl_easy_setopt(eh, CURLOPT_XFERINFODATA, &unhandledTask);
+#else
+		curl_easy_setopt(eh, CURLOPT_PROGRESSFUNCTION, older_progress);
+		curl_easy_setopt(eh, CURLOPT_PROGRESSDATA, &unhandledTask);
+#endif
+
+		curl_easy_setopt(eh, CURLOPT_HEADER, 0L);
+		curl_easy_setopt(eh, CURLOPT_URL, unhandledTask.Url().c_str());
+		unhandledTask.Unhandled(false);
+		curl_multi_add_handle(cm, eh);
+	}
+
+	void Excutor()
+	{
+
+		CURLM *cm = nullptr;
+		CURLMsg *msg;
+		long L;
+		int M, Q, U = -1;
+		fd_set R, W, E;
+		struct timeval T;
+		CURLMcode code;
+		cm = curl_multi_init();
+		int MAX_SIZE = 9;
+		/* we can optionally limit the total amount of connections this multi handle uses */
+		curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, MAX_SIZE);
+
+		while (true)
+		{
+			while (!g_taskQueue.HasUnhandledTask())
+			{
+				Sleep(100);
+			}
+
+
+			M = Q = U = -1;
+
+			int initNum = 0;
+			while (g_taskQueue.HasUnhandledTask() && initNum++ < MAX_SIZE)
+			{
+				init(cm);
+			}
+			while (U) {
+				//when running_handles is set to zero (0) on the return of this function, there is no longer any transfers in progress
+				curl_multi_perform(cm, &U);
+				//when U==0, all in finished
+				if (U) {
+					FD_ZERO(&R);
+					FD_ZERO(&W);
+					FD_ZERO(&E);
+					if (code = curl_multi_fdset(cm, &R, &W, &E, &M)) {
+						fprintf(stderr, "%s/n", curl_multi_strerror(code));
+						return;
+					}
+					//An application using the libcurl multi interface should call curl_multi_timeout to figure out how long it should wait for socket actions - at most - before proceeding
+					if (code = curl_multi_timeout(cm, &L)) {
+						fprintf(stderr, "%s/n", curl_multi_strerror(code));
+						return;
+					}
+					//optimum solution for next time timeout
+					if (L == -1)
+						L = 100;
+					if (M == -1) {
+						Sleep(L);
+					}
+					else {
+						T.tv_sec = L / 1000;
+						T.tv_usec = (L % 1000) * 1000;
+						//The select() system call examines the I/O descriptor sets whose addresses are passed	in readfds, writefds, and exceptfds to see if some of their
+						//	descriptors are ready for reading, are ready for writing, or have an exceptional condition pending, respectively
+						if (0 > select(M + 1, &R, &W, &E, &T)) {
+							fprintf(stderr, "E: select(%i,,,,%li): %i: %s\n",
+								M + 1, L, errno, strerror(errno));
+							return;
+						}
+					}
+				}
+
+				while ((msg = curl_multi_info_read(cm, &Q))) {
+					Task *task;
+					curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &task);
+					task->CurlCode(msg->data.result);
+
+					CURL *e = msg->easy_handle;
+					curl_multi_remove_handle(cm, e);
+					curl_easy_cleanup(e);
+
+					/*Execute action indicate by user*/
+					task->Action()->Do(*task);
+					g_taskQueue.Pop(task->Mark());
+				}
+				if (g_taskQueue.HasUnhandledTask()) {
+					init(cm);
+					U++; /* just to prevent it from remaining at 0 if there are more URLs to get */
+				}
+			}
+		}
+	}
+
+	void Router::Get(const std::string &url, Action *httpAction, void * userData /*=-1*/)
+	{
+		if (std::find(std::begin(actionList), std::end(actionList), httpAction) != std::end(actionList))
+		{
+			actionList.push_back(httpAction);
+		}
+		Run(Task(url, httpAction, userData));
+	}
+
+	void Router::Post(const std::string &url, const std::vector<UploadedData> & uploadedDatas, Action *httpAction, void *userData /*= nullptr*/)
+	{
+		if (std::find(std::begin(actionList), std::end(actionList), httpAction) != std::end(actionList))
+		{
+			actionList.push_back(httpAction);
+		}
+		Run(Task(url, uploadedDatas, httpAction, userData));
+	}
+
+	void Router::Run(Task &&task)
+	{
+		g_taskQueue.Push(std::move(task));
+		if (g_createdExcutor == true)
+		{
+			g_createdExcutor = false;
+			std::thread excutor(Excutor);
+			excutor.detach();
+		}
+	}
+
+	Router::~Router()
+	{
+		std::for_each(std::begin(actionList), std::end(actionList), [](Action *action) {
+			delete action;
+		});
+	}
+
+
+	UploadedData::UploadedData(FIELD dataType, const std::string &key, const std::string &value) :field(dataType), key(key), value(value)
+	{
+
+	}
+
+
+	UploadedData::UploadedData(const UploadedData &postedData) :field(postedData.field), key(postedData.key), value(postedData.value)
+	{
+
+	}
+
+	UploadedData::UploadedData(UploadedData &&postedData) : field(postedData.field), key(std::move(postedData.key)), value(std::move(postedData.value))
+	{
+
+	}
+
+
+	NETWORK_API UploadedData & UploadedData::operator=(const UploadedData &uploadData)
+	{
+		field = uploadData.Field(); key = uploadData.Key(), value = uploadData.Value();
+		return *this;
+	}
+
+	NETWORK_API bool UploadedData::operator==(const UploadedData &postData) const
+	{
+		return field == postData.field && key == postData.key&&value == postData.value;
+	}
+
+	NETWORK_API UploadedData::FIELD UploadedData::Field() const
+	{
+		return field;
+	}
+
+	NETWORK_API	void UploadedData::Field(FIELD val)
+	{
+		field = val;
+	}
+
+
+	NETWORK_API const std::string & UploadedData::Key() const
+	{
+		return key;
+	}
+
+
+	NETWORK_API void UploadedData::Key(const std::string &val)
+	{
+		key = val;
+	}
+
+
+	NETWORK_API const std::string & UploadedData::Value() const
+	{
+		return value;
+	}
+
+
+	NETWORK_API void UploadedData::Value(const std::string &val)
+	{
+		value = val;
+	}
+
+}
---
+++ Network/Url.cpp
@@ -0,0 +1,66 @@
+﻿#include "stdafx.h"
+#include <sstream>
+#include <random>
+#include <array>
+#include "Url.h"
+
+namespace Http {
+Url::Url(const std::string& host, const std::string& path, const AttribMap& queryString) : scheme("http"), host(host), path(path), queryString(queryString) {
+    FormatUrl();
+}
+
+
+Url::Url(const char* url)
+    : url(url) {
+}
+
+
+Url::Url(const std::string& url)
+    : url(url) {
+
+}
+
+const std::string& Url::String() const {
+    return url;
+}
+
+
+NETWORK_API const Url::AttribMap& Url::GetAttribMap() const {
+    return queryString;
+}
+
+void Url::FormatUrl() {
+    url.reserve((host.length() + path.length()) * 5);
+    url = scheme + "://" + host + path + "?";
+
+    for (auto const& attribPair : queryString) {
+        url += (attribPair.first + "=" + attribPair.second);
+        url.append("&");
+    }
+    url.pop_back();
+}
+
+Url::~Url() {
+}
+
+
+Url::AttribMap::AttribMap(std::initializer_list<value_type> init) : _AttribMap(init) {
+}
+
+
+NETWORK_API Url::AttribValue& Url::AttribMap::operator[](const AttribKey& key) {
+    return _AttribMap::operator[](key);
+}
+
+NETWORK_API const std::string Url::AttribMap::ToString() const {
+    std::string _attribs;
+    size_type increment = 0;
+    for (auto const& attribPair : *this) {
+        _attribs += (attribPair.first + "=" + attribPair.second);
+#define LAST_ELEMENT_INDEX (size()-1)
+        if (increment++ < LAST_ELEMENT_INDEX) _attribs.append("&");
+    }
+    return _attribs;
+}
+
+}
